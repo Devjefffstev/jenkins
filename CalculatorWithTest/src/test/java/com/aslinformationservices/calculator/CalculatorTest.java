6fef7bfe60b72a44b577e83a971afc037bcdf746
Jefferson Soto <jefferson_soto@epam.com>
2025-05-07 08:54:46 -0500

    add sleep calculator

--- CalculatorWithTest/src/test/java/com/aslinformationservices/calculator/CalculatorTest.java
+++ CalculatorWithTest/src/test/java/com/aslinformationservices/calculator/CalculatorTest.java
@@ -10,6 +10,12 @@ class CalculatorTest {
 	    void testAdd() {
 	        Calculator calculator = new Calculator();
 	        assertEquals(5.0, calculator.add(2.0, 3.0), DELTA);
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+				fail("Test interrupted");
+			}
 	    }
 
 	    @Test
